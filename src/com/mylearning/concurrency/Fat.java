2cf256f4a05d63cd3ae594865bbb29c67e15a8ae
I034966 <I034966@PVGN56583944A.apj.global.corp.sap>
2018-08-20 18:34:41 +0800

    Update

---
+++ src/com/mylearning/concurrency/Fat.java
@@ -0,0 +1,15 @@
+package com.mylearning.concurrency;
+
+public class Fat {
+	private volatile double d;
+	private static int counter = 0;
+	private final int id = counter++;
+	public Fat() {
+		// Expensive, interruptible operation:
+		for(int i = 0; i < 10000; i++) {
+			d += (Math.PI + Math.E) / (double)i;
+		}
+	}
+	public void operation() { System.out.println(this); }
+	public String toString() { return "Fat id: " + id; }
+}
