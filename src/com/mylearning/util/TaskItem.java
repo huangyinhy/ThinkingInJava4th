1d738eca8d0b7c1504d8140df9ad2f0d2cf3d572
I034966 <I034966@PVGN56583944A.apj.global.corp.sap>
2018-08-31 19:05:31 +0800

    201808311905

---
+++ src/com/mylearning/util/TaskItem.java
@@ -0,0 +1,11 @@
+package com.mylearning.util;
+import java.util.concurrent.*;
+
+public class TaskItem<R,C extends Callable<R>> {
+	public final Future<R> future;
+	public final C task;
+	public TaskItem(Future<R> future, C task) {
+		this.future = future;
+		this.task = task;
+	}
+}
